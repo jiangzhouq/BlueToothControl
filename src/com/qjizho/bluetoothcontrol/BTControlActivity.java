88ea0e5216a3c829bfbc9a765f500232527e107f
jiangzhouq <jiangzhouq@gmail.com>
2014-09-03 16:27:04 +0800

    修改界面逻辑，增加右上方状态图标

--- src/com/qjizho/bluetoothcontrol/BTControlActivity.java
+++ src/com/qjizho/bluetoothcontrol/BTControlActivity.java
@@ -8,12 +8,21 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.Animation;
@@ -29,6 +38,8 @@ public class BTControlActivity extends Activity implements OnClickListener{
 	private ConnectedThread mConnectedThread;
 	private ConnectAsyncTask task;
 	ImageView img;
+	private ImageView blueState;
+	private View mDecorView;
 	private TextView txt;
 	PowerManager pManager;
 	WakeLock mWakeLock;
@@ -39,11 +50,93 @@ public class BTControlActivity extends Activity implements OnClickListener{
 		setContentView(R.layout.activity_main);
 		img = (ImageView) findViewById(R.id.img);
 		img.setOnClickListener(this);
+		blueState = (ImageView) findViewById(R.id.blue_state);
+		blueState.setOnClickListener(this);
 		txt = (TextView) findViewById(R.id.txt);
-//		Button btn2 = (Button) findViewById(R.id.btn2);
-//		btn2.setOnClickListener(this);
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		int sensorType = Sensor.TYPE_ACCELEROMETER;
+		sm.registerListener(myAccelerometerListener,sm.getDefaultSensor(sensorType),SensorManager.SENSOR_DELAY_NORMAL);
+		
+		mDecorView = getWindow().getDecorView();
+		hideSystemUI();
 	}
+	private void changeScreenState(int state){
+		switch(state){
+		case 0:
+			img.setVisibility(View.GONE);
+			txt.setVisibility(View.GONE);
+			blueState.setVisibility(View.VISIBLE);
+			break;
+		case 1:
+			img.setVisibility(View.VISIBLE);
+			txt.setVisibility(View.VISIBLE);
+			blueState.setVisibility(View.INVISIBLE);
+			break;
+		}
+	}
+	private int mSensorCount = 0;
+	final SensorEventListener myAccelerometerListener = new SensorEventListener(){  
+        
+        //复写onSensorChanged方法  
+        public void onSensorChanged(SensorEvent sensorEvent){  
+            if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){  
+                //图解中已经解释三个值的含义  
+                float X_lateral = sensorEvent.values[0];  
+                float Y_longitudinal = sensorEvent.values[1];  
+                float Z_vertical = sensorEvent.values[2];  
+//                if(X_lateral > 5)
+//                	Log.d("qiqi","\n heading "+X_lateral); 
+                if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE  ){
+                	if(Y_longitudinal > 3 ){
+                		if(X_lateral > 0){
+                			mSensorCount ++;
+                			if(mSensorCount == 10){
+                				mSensorCount = 0;
+                				if(null != mConnectedThread){
+                					Log.d("qiqi", "send hello.");
+                					mConnectedThread.write(new String("right").getBytes());
+                				}
+                			}
+                		}else if(X_lateral < 0){
+                			mSensorCount ++;
+                			if(mSensorCount == 10){
+                				mSensorCount = 0;
+                				if(null != mConnectedThread){
+                					Log.d("qiqi", "send hello.");
+                					mConnectedThread.write(new String("left").getBytes());
+                				}
+                			}
+                		}
+                	}else if (Y_longitudinal < -3){
+                		if(X_lateral > 0){
+                			mSensorCount ++;
+                			if(mSensorCount == 10){
+                				mSensorCount = 0;
+                				if(null != mConnectedThread){
+                					Log.d("qiqi", "send hello.");
+                					mConnectedThread.write(new String("left").getBytes());
+                				}
+                			}
+                		}else{
+                			mSensorCount ++;
+                			if(mSensorCount == 10){
+                				mSensorCount = 0;
+                				if(null != mConnectedThread){
+                					Log.d("qiqi", "send hello.");
+                					mConnectedThread.write(new String("right").getBytes());
+                				}
+                			}
+                		}
+                	}
+                }
+            }  
+        }  
+        //复写onAccuracyChanged方法  
+        public void onAccuracyChanged(Sensor sensor , int accuracy){  
+            Log.d("qiqi", "onAccuracyChanged");  
+        }  
+    };  
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -97,12 +190,12 @@ public class BTControlActivity extends Activity implements OnClickListener{
 				connect_state = true;
 			}
 			break;
+		case R.id.blue_state:
+			changeScreenState(1);
+			break;
 //		case R.id.btn2:
 //			Log.d("qiqi", "send hello.");
-//			if(null != mConnectedThread){
-//				Log.d("qiqi", "send hello.");
-//				mConnectedThread.write(new String("hello").getBytes());
-//			}
+			
 //			break;
 		}
 	}
@@ -138,6 +231,7 @@ public class BTControlActivity extends Activity implements OnClickListener{
 				img.setImageResource(R.drawable.bt_connected);
 				txt.setText(R.string.bt_conneced);
 				connect_state = true;
+				changeScreenState(0);
 			}else{
 				img.clearAnimation();
 				img.setImageResource(R.drawable.bt_connecting);
@@ -168,5 +262,30 @@ public class BTControlActivity extends Activity implements OnClickListener{
 	        }
 		}
 		
+	}
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+	        super.onWindowFocusChanged(hasFocus);
+	    if (hasFocus) {
+	    	mDecorView.setSystemUiVisibility(
+	                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+	}
+	// This snippet hides the system bars.
+	private void hideSystemUI() {
+	    // Set the IMMERSIVE flag.
+	    // Set the content to appear under the system bars so that the content
+	    // doesn't resize when the system bars hide and show.
+	    mDecorView.setSystemUiVisibility(
+	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+	            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+	            | View.SYSTEM_UI_FLAG_IMMERSIVE);
 	}
 }
